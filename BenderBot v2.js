3bc72d111625835477be579f77c888235b85eab8
javirexu97 <34272418+javirexu97@users.noreply.github.com>
2019-01-15 17:23:36 +0100

    Add files via upload
    Nueva versión del bot con nuevos comandos ( D4, D6 Y D20 ) para lanzar dados de 4, 6 y 20 caras respectivamente

---
+++ BenderBot v2.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+ 
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+ 
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("me cago en tus putos muertos!");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("D6")) {
+    var num;
+    num=Math.trunc((Math.random()*6)+1);
+    
+    message.channel.send(num);
+  }
+});
+ 
+   client.on("message", (message) => {
+    if (message.content.startsWith("D4")) {
+      var num;
+      num = Math.trunc((Math.random() * 4) + 1);
+      message.channel.send(num);
+    }
+  });
+
+   
+  client.on("message", (message) => {
+    if (message.content.startsWith("D20")) {
+      var num;
+      num =Math.trunc((Math.random() * 20) + 1);
+      message.channel.send(num);
+    }
+  });
+
+client.login("NTM0MzIwMjc1NzEwNDEwNzY2.Dx344g.i-pQZFo7Dx93XVjjQ9rcZsyCi5k");
